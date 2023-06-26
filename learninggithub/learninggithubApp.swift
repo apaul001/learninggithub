d67cfabacda3fa8cff9151feea669e3d50523992
Ally Paul <aepaul0356@gmail.com>
2023-06-26 15:29:14 -0400

    Initial Commit

---
+++ learninggithub/learninggithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learninggithubApp.swift
+//  learninggithub
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct learninggithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
